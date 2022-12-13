8231d73191659990af444128e2d0d79da62231b0
sarbbb <83230342+sarbbb@users.noreply.github.com>
2022-12-13 15:26:00 +0800

    update

--- DDL.sql
+++ DDL.sql
@@ -1,6 +1,12 @@
+CREATE TABLE study  #建置資料表 先寫欄位名稱再寫dtype
+(
+	ID NUMERIC(10,0) PRIMARY KEY,
+    STUDENT_NAME VARCHAR(255)
+);
 DROP TABLE study;  #刪除資料表
 TRUNCATE TABLE study;  #刪除資料表
-DELETE FROM study;  #刪除資料表 
+DELETE FROM study;  #刪除資料表 # MySQL預設safe mode，沒有下where條件無法執行此語句ALTER，需要到Edit->Preferences->SQL Editor->Other 把Safe Updates的勾勾給取消
+
 /*
 DELETE FROM table_name，是DML語法，主要用於刪除表內的records，沒有下where條件就是刪除整張表的records
 */
@@ -12,9 +18,21 @@ DELETE FROM table_name，是DML語法，主要用於刪除表內的records，沒
 •	truncate：刪除表的內容，表的結構存在，可以釋放空間,沒有備份表之前要慎用；
 
 執行速度：drop > truncate > delete
+
+資料參考：https://www.runoob.com/
 */
-CREATE TABLE study  #建置資料表 先寫欄位名稱再寫dtype
-(
-	ID NUMERIC(10,0) PRIMARY KEY,
-    STUDENT_NAME VARCHAR(255)
+
+CREATE TABLE GEOGRAPHY (  
+	GEOGRAPHY_ID NUMERIC (10,0) PRIMARY KEY, 
+	REGION_NAME  VARCHAR(255)
+);
+
+CREATE TABLE STORE_INFORMATION(
+	STORE_ID   NUMERIC (10,0) PRIMARY KEY,
+	STORE_NAME VARCHAR(255),
+	SALES      NUMERIC (10,0) CHECK (SALES > 0),
+	STORE_DATE datetime,
+-- 	GEOGRAPHY_ID NUMERIC(10,0) REFERENCES GEOGRAPHY(GEOGRAPHY_ID)
+ 	GEOGRAPHY_ID NUMERIC (10,0),
+ 	CONSTRAINT CONSTRAINT_GEOGRAPHY_ID FOREIGN KEY (GEOGRAPHY_ID) REFERENCES GEOGRAPHY (GEOGRAPHY_ID)
 );
